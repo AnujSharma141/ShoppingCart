8f37f803d80fbd2898019ec24c6fdabf5accd053
Anuj Sharma <19ucc109@lnmiit.ac.in>
2019-12-11 22:10:28 +0530

    multiple times cart-items adding functionality

--- index.js
+++ index.js
@@ -2,30 +2,46 @@
 let sample1 = {
   name: "Sample 1",
   price: 300,
-  access: document.querySelector("#sample1"),
-  core: 0
+  core: 0,
+  access: document.querySelector("#sample1").onclick = () =>{
+    tempo(0);
+    }
 };
+
 let sample2 = {
   name: "Sample 2",
   price: 400,
-  access: document.querySelector("#sample2"),
-  core: 0
+  core: 0,
+  access: document.querySelector("#sample2").onclick = () =>{
+    tempo(1);
+    }
 };
+
 let sample3 = {
   name: "Sample 3",
   price: 500,
-  access: document.querySelector("#sample3"),
-  core: 0
+  core: 0,
+  access: document.querySelector("#sample3").onclick = () =>{ 
+    tempo(2);  
+  }
 };
+
+//conditional function calls
+const tempo = (k) => {
+  if(bigData[k].core != 0){alert(`already added bitch`);}
+  if(bigData[k].core === 0){buy(k);}
+}
 
 let countIndex = 0;
 let finalAmount = 0;
-//filling out the props
+//filling out the props 
 let bigData = [sample1, sample2, sample3];
+
 
 //appending the cart
 const buy = i => {
   countIndex++;
+  finalAmount = finalAmount + bigData[i].price; 
   let cartValue = document.getElementById("cart-value");
   cartValue.style.cssText =
     " display:flex; animation: jump 0.3s 1; background:#b30000; transition:0.3s; ";
@@ -37,40 +53,15 @@ const buy = i => {
   itemName.innerHTML = bigData[i].name;
   pal.appendChild(itemName);
   let itemNo = document.createElement("div");
-  itemNo.innerHTML = bigData[i].core;
+  itemNo.innerHTML = `Quantity : ${bigData[i].core}`;
   pal.appendChild(itemNo);
   let itemPrice = document.createElement("div");
-  itemPrice.innerHTML = bigData[i].price;
+  itemPrice.innerHTML = `Amount : ${bigData[i].price}`;
   pal.appendChild(itemPrice);
   document.querySelector(".item-section").appendChild(pal);
   document.querySelector(".amount-int").innerHTML = finalAmount;
 };
 
-//buy function call
-bigData[0].access.onclick = function() {
-  finalAmount = finalAmount + bigData[0].price;
-  if (bigData[0].core == 0) {
-    buy(0);
-  } else {
-    bigData[0].core++;
-  }
-};
-bigData[1].access.onclick = function() {
-  finalAmount = finalAmount + bigData[1].price;
-  if (bigData[1].core == 0) {
-    buy(1);
-  } else {
-    bigData[1].core++;
-  }
-};
-bigData[2].access.onclick = function() {
-  finalAmount = finalAmount + bigData[2].price;
-  if (bigData[2].core == 0) {
-    buy(2);
-  } else {
-    bigData[2].core++;
-  }
-};
 
 //cart window
 const shell = document.getElementById("window");
